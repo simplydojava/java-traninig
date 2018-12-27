e1b43fef01852b7a347226c8492f6a3b0ed93f03
AsmaYasmeen <yasmeen.asma30@gmail.com>
2018-12-26 23:11:05 -0500

    java program to print numbers from 1 to 1000 using for loop.

---
+++ src/com/java/forExample2/ForExample2.java
@@ -0,0 +1,14 @@
+package com.java.forExample2;
+
+public class ForExample2 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=1000;i++)
+		{
+			System.out.println("print the values of i: "+i);
+		}
+
+	}
+
+}
