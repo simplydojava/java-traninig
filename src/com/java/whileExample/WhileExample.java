87f007927c3aa505bbe41200b39d4c166644846e
AsmaYasmeen <yasmeen.asma30@gmail.com>
2018-12-26 23:13:03 -0500

    java program using while loop.

---
+++ src/com/java/whileExample/WhileExample.java
@@ -0,0 +1,13 @@
+package com.java.whileExample;
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+		int a=1;
+		while(a<=4) {
+			System.out.println("value of a is "+a);
+			a++;
+		}
+	}
+
+}
